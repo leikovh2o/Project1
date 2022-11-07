efd41d2ed63ad46028df4c8d9aa1f976f920296c
Ion <ioncazacu13@gmail.com>
2022-11-07 10:49:52 +0200

    Added lottie files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerModule } from './shared/layouts/banner.module';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+import { SplashScreenComponent } from './shared/components/splash-screen/splash-screen.component';
+
+export function playerFactory(): any {  
+  return import('lottie-web');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BannerModule
+    BannerModule,
+    LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
